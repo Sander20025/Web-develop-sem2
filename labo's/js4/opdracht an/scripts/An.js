5757ef21dbbbe8da73d47ef3c8bdb929cc2ed815
Sander20025 <sander.louagie@student.vives.be>
2022-03-20 11:57:23 +0100

    an

---
+++ labo's/js4/opdracht an/scripts/An.js
@@ -0,0 +1,31 @@
+const setup = () => {
+let tekst = document.getElementById("manVanAn").textContent.toLowerCase();
+
+let aantal = document.getElementsByClassName("hoeveelheid");
+
+//==================================================================================
+// Via indexOf
+let teller = 0;
+let index = tekst.indexOf("an");
+while(index >= 0) {
+        teller ++;
+        index ++;
+        index = tekst.indexOf("an", index);
+}
+    console.log(teller);
+    aantal[0].innerHTML= teller;
+//==================================================================================
+// Via lastIndexOf
+    let teller2 = 0;
+    let index2 = tekst.lastIndexOf("an");
+    while(index2 >= tekst.indexOf("an")){
+        index2 --;
+        index2 = tekst.lastIndexOf("an", index2);
+        teller2 ++;
+    }
+    console.log(teller2);
+    aantal[1].innerHTML = teller2;
+}
+
+
+window.addEventListener("load", setup);
